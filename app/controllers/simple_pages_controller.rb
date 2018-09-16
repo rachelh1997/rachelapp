2ba811bdd0efca860af231e3c09fddd387de8bbd
Rachel H <notoriouslyanxious1111@gmail.com>
2018-09-16 22:19:29 +0000

    adding contact and about page

---
+++ app/controllers/simple_pages_controller.rb
@@ -0,0 +1,9 @@
+class SimplePagesController < ApplicationController
+  def about
+  end
+
+  def contact
+  end
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'simple_pages#index'
+  get 'simple_pages/about'
+  get 'simple_pages/contact'
+  get 'simple_pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
